4cfa87b09c7ec0547421e87d8f5b0ae144eb539c
Boyko-Anna <boyko.anna.108@gmail.com>
2019-07-19 20:11:58 +0300

    Add initial config files and project's files

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+    src: path.join(__dirname, "../src"),
+    dist: path.join(__dirname, "../dist"),
+    assets: "assets/"
+}
+
+module.exports = {
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: "/"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: "babel-loader",
+            exclude: "/node_modules/"
+        },
+        {
+            test: /\.(png|jpg|gif|svg|webp)$/,
+            loader: "file-loader",
+            options: {
+                name: "[name].[ext]"
+            }
+        },
+        {
+            test: /\.css$/,
+            use: [
+                "style-loader",
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development',
+                    }
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }
+            ],
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+            chunkFilename: '[id].css',
+            ignoreOrder: false
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: "./index.html"
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to: `${PATHS.assets}img/` },
+            { from: `${PATHS.src}/static`, to: "" }
+        ])
+    ]
+}
